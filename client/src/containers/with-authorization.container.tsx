068df7cf2b1b50c133d01b7cf259b35ed921a2c1
Jono <jonathanphame@gmail.com>
2020-10-15 14:03:34 +1100

    Add cleanup function for with-authorization
    Need cleanup to unsubscribe to firebase listener.
    
    Also, Removed the isloading state because this could potentially be handled outside the component

--- client/src/containers/with-authorization.container.tsx
+++ client/src/containers/with-authorization.container.tsx
@@ -6,26 +6,29 @@ import * as ROUTES from "../routes";
 const withAuthorization = (
   condition: (authUser: firebase.User | null) => boolean
 ) => (component: JSX.Element) => {
-  const [isLoading, setIsLoading] = useState(true);
   const history = useHistory();
-  const firebaseContext = useContext(FirebaseContext);
+  const firebase = useContext(FirebaseContext);
 
   useEffect(() => {
     console.log("Within authorization");
-    firebaseContext?.auth.onAuthStateChanged((authUser) => {
+
+    const listener = firebase?.auth.onAuthStateChanged((authUser) => {
       if (!condition(authUser)) {
         console.log("User is not authorized for this route");
         history.push(ROUTES.SIGN_UP);
       } else {
         console.log("User is authorized");
       }
-      setIsLoading(false);
     });
+
+    return () => {
+      if (listener !== undefined) listener();
+    };
   });
 
   // Cannot use UserContext to validate auth state because dispatch occurs
-  // asynchronously on next re-render and will not be effective immediately 
-  return !isLoading ? component : null;
+  // asynchronously on next re-render and will not be effective immediately
+  return component;
 };
 
 export default withAuthorization;

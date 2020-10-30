d3b13f58b4df64f163d2cc6db4d32aae8b7b28a7
Jono <jonathanphame@gmail.com>
2020-10-30 13:06:34 +1100

    Add useful helpers for testing

--- client/src/test-helper.ts
+++ client/src/test-helper.ts
@@ -1,13 +1,24 @@
-import { fireEvent } from "@testing-library/react";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-const changeInputValueByEl = (el: HTMLInputElement, value: string) => fireEvent.change(el, { target: { value: value } });
+const changeInputValueByEl = (el: HTMLInputElement, value: string) => userEvent.type(el, value);
 const changeInputByTestId = (fn: (match: string) => HTMLElement) => (testId: string) => (value: string) => {
   const element = (getInputFieldsByTestId(fn)(testId)[0] as HTMLInputElement);
-  fireEvent.change(element, { target: { value: value } });
+  userEvent.type(element, value);
   return element;
 }
 
+const typeIntoLabelField = (label: string, value: string) => {
+  const element = screen.getByLabelText(new RegExp(label,"i"));
+  userEvent.type(element, value);
+  return element;
+}
+const typeIntoTextBox = (textboxName: string, value: string) => {
+  const element = screen.getByRole("textbox", {name:textboxName});
+  userEvent.type(element, value);
+  return element;
+}
 const getInputFieldByTestId = (fn: (match: string) => HTMLElement) => (testId: string) => getInputFieldsByTestId(fn)(testId)[0] as HTMLInputElement;
 const getInputFieldsByTestId = (fn: (match: string) => HTMLElement) => (testId: string) => fn(testId).getElementsByTagName("INPUT");
 
-export { changeInputValueByEl, changeInputByTestId, getInputFieldByTestId, getInputFieldsByTestId };
+export { changeInputValueByEl, changeInputByTestId, getInputFieldByTestId, getInputFieldsByTestId, typeIntoTextBox, typeIntoLabelField };
